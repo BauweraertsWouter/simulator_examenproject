87cb0a64486a898ac175a95b5eb84b223814c0a4
BauweraertsWouter <wouter.bauweraerts@telenet.be>
2017-06-03 11:39:22 +0200

    initial commit

---
+++ src/main/java/be/kdg/se3/wbw/examenproject/simulator/simulator/SimulatorApplication.java
@@ -0,0 +1,12 @@
+package be.kdg.se3.wbw.examenproject.simulator.simulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SimulatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SimulatorApplication.class, args);
+	}
+}
